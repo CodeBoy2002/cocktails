161aa8905f72f11da93f00a1b2886d9f362fabc4
Sumit <marvelsumit2002@gmail.com>
2023-09-17 22:00:35 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about page</h2>
+      <p>
+        The origins of the word "cocktail" have been debated (see section §
+        Etymology). The first written mention of "cocktail" as a beverage
+        appeared in The Farmers Cabinet, 1803 in the United States. The first
+        definition of a cocktail as an alcoholic beverage appeared three years
+        later in The Balance and Columbian Repository (Hudson, New York) May 13,
+        1806.[4] Traditionally, cocktail ingredients included spirits, sugar,
+        water and bitters,[5] however, this definition evolved throughout the
+        1800s, to include the addition of a liqueur.
+      </p>
+    </section>
+  );
+};
+
+export default About;
